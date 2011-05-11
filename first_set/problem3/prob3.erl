56099ec7fa1e1e65336755cd6c6fe417b0639fe6
Tomas Henriquez <hassek@kentriki.com>
2011-05-10 20:46:37 -0430

    problema 3 listo

--- first_set/problem3/prob3.erl
+++ first_set/problem3/prob3.erl
@@ -1,18 +1,24 @@
 -module(prob3).
--export([get_prime_factor/1, get_prime_factor/2]).
+-export([main/1]).
 
-get_prime_factor(X) ->
+main(Val) ->
+    get_prime_factor({Val, 2}).
+
+get_prime_factor({Val, X}) when Val rem X == 0 ->
+    io:format("valor: ~w, mod: ~w~n", [Val, X]),
+    get_prime_factor({Val div X, X});
+
+get_prime_factor({Val, X}) when Val == 1 ->
+    get_prime_factor({X});
+
+get_prime_factor({Val, X}) ->
     if 
         X rem 2 == 0 ->
-            get_prime_factor(X, X-1).
+            get_prime_factor({Val, X+1});
         true ->
-            get_prime_factor(X, X-2)
-    end.
+            io:format(" true -- valor: ~w, mod: ~w~n", [Val, X]),
+            get_prime_factor({Val, X+2})
+    end;
 
-get_prime_factor(Val, X) ->
-    if 
-        Val rem X == 0 ->
-            X
-    end.
-
-    get_prime_factor(Val, X-2);
+get_prime_factor({X}) ->
+    X.
