69373d12eb28333801148437930db5bba73419f0
Tomas Henriquez <hassek@kentriki.com>
2011-05-12 01:10:30 -0430

    mejora al problema4

--- first_set/problem4/prob4.erl
+++ first_set/problem4/prob4.erl
@@ -3,26 +3,26 @@
 %Carajo este si me costo haha
 
 palindrome(X) ->
-    find_palindrome(X),
+    find_palindrome({X*X,X}),
     ok.
 
 
-find_palindrome(Pal) when Pal > 1 ->
-    {X, Res, Length} = factor_palindrome(Pal),
+find_palindrome({Pal, Max}) when Pal > 1 ->
+    {X, Res, Length} = factor_palindrome({Pal,Max}),
     if 
         Length == 2 ->
             io:format("Para ~w los valoes son ~w~n", [X, Res]);
         true ->
             io:format("Intentando ~w~n", [Pal-1]),
-            find_palindrome(Pal-1)
+            find_palindrome({Pal-1, Max})
     end.
 
-factor_palindrome(Pal) ->
+factor_palindrome({Pal,Max}) ->
     Val = integer_to_list(Pal),
     Val2 = lists:reverse(Val),
     if
         Val == Val2 ->
-            Result = factor_palindrome(999,{Pal, [], 0});
+            Result = factor_palindrome(Max,{Pal, [], 0});
         true ->
             Result = {false, [], 0}
     end,
