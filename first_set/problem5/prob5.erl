e60354d6512556bcddd8ea547d52e4fb932c71f4
Tomas Henriquez <hassek@kentriki.com>
2011-05-12 18:47:38 -0430

    problem 5 resuelto programaticamente

--- first_set/problem5/prob5.erl
+++ first_set/problem5/prob5.erl
@@ -1,15 +1,34 @@
 -module(prob5).
--export([divisible_by_all/1]).
+-export([get_primes/1]).
 
-get_primes({Max, Primes}) -> % me da los primos menores o iguales a Max
-    
+get_primes(Max) -> % me da los primos menores o iguales a Max
+    get_min_common_div(Max, get_primes(Max, 3, [2]), []).
 
 
-divisible_by_all(Max) ->
-    divisible_by_all(Max, 1).
+get_primes(Max, Num, Primes) when Max < Num -> 
+    Primes;
 
-divisible_by_all(Max, Res) when Max == 1->
-    Res;
+get_primes(Max, Num, Primes) -> 
+    Val = not lists:any(fun(X) -> Num rem X == 0 end, Primes),
+    if 
+        not Val ->
+            get_primes(Max, Num+2, Primes);
+        true ->
+            get_primes(Max, Num+2, [Num|Primes])
+    end.
 
-divisible_by_all(Max, Res) ->
-    divisible_by_all(Max-1, Res*Max).
+get_min_common_div(_, [], Totals) ->
+    %Totals;
+    lists:foldl(fun(X,Y) -> X*Y end,1, Totals);
+
+get_min_common_div(Max, [Prime|Primes], Totals) ->
+    get_min_common_div(Max, Primes, [get_max_common_div(Max, Prime, 1)| Totals]).
+
+get_max_common_div(Max, Base, Num) when Base * Num =< Max ->
+    get_max_common_div(Max, Base, Base * Num);
+
+get_max_common_div(_, _, Num) ->
+    Num.
+
+
+
