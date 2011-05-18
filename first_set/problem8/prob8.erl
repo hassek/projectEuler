5d7700457a072204c5a5edce4de59bf47bfb3db3
Hassek <nullwriter@gmail.com>
2011-05-18 00:56:34 -0430

    prob8 listo.

--- first_set/problem8/prob8.erl
+++ first_set/problem8/prob8.erl
@@ -1,5 +1,20 @@
 -module(prob8).
--export([greatest_product/1]).
+-export([greatest_product/2]).
 
-greatest_product(X) ->
-    Nums
+greatest_product(X, Y) ->
+    greatest_product({X,Y-1,0}).
+
+greatest_product({[F|X], Y, Max}) ->
+    Candidate = lists:foldr(fun(X,Y) -> (X-48) * Y end, 1, [F|string:sub_string(X,1,Y)]),
+    %io:format("-- ~w -- ~n",[Candidate]),
+
+    if 
+        Max < Candidate ->
+            greatest_product({X, Y, Candidate});
+        true ->
+            greatest_product({X, Y, Max})
+    end;
+
+greatest_product({[], _, Max}) ->
+    Max.
+
